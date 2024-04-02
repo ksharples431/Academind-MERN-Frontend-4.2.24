dbb0c4396a184900cbe6102b1df7d40d90461f78
Katie Sharples <KSharples431@gmail.com>
2024-04-02 18:30:11 -0400

    Navigation added

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -4,7 +4,12 @@ import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
-    { id: 'u1', name: 'Katie', image: 'image', places: 3 }
+    {
+      id: 'u1',
+      name: 'Katie',
+      image: 'https://avatars.githubusercontent.com/u/48837909?v=4',
+      places: 3,
+    },
   ];
   return <UsersList items={USERS} />;
 };
